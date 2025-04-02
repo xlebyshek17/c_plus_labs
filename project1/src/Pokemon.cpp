c10a283a80020e61e755c287285d5e8f721fb1a2
xlebyshek17 <kristina1111113@gmail.com>
2025-04-02 13:16:10 +0200

    change printInfo() method

--- project1/src/Pokemon.cpp
+++ project1/src/Pokemon.cpp
@@ -2,13 +2,17 @@
 #include "Team.h"
 #include "iostream"
 
-Pokemon::Pokemon(const std::string& _name, Team* _team) : Pokemon(name)
+Pokemon::Pokemon(const std::string& _name, Team* _team) : Pokemon(_name)
 {
+    team = _team;
     if (team)
         team->addPokemon(*this);
     }
 
 void Pokemon::printInfo() const
 {
-    std::cout << "Pokemon: " << name << " (Type: " << type << ")" << std::endl;
+    if (team)
+        std::cout << "Pokemon: " << name << " (Team: " << team->getName() << ")\n";
+    else
+        std::cout << "Pokemon: " << name << std::endl;
 }
